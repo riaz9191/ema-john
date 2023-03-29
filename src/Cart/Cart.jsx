b6f84c371b25e44ae9d6575b6be928758d0e451e
riaz9191 <riazahammed9191@gmail.com>
2023-03-30 00:29:36 +0600

    added products and calculation

---
+++ src/Cart/Cart.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Cart.css";
+const Cart = ({ cart }) => {
+  // const cart= props.cart
+  //   const { cart } = props;
+  console.log(cart);
+  let totalPrice = 0;
+  let totalShipping = 0;
+  
+  for (const product of cart) {
+    totalPrice = totalPrice + product.price;
+    totalShipping = totalShipping + product.shipping;
+  }
+  const tax = totalPrice *7 /100;
+  const grandTotal = totalPrice + totalShipping + tax;
+  const gp = grandTotal.toFixed(2);
+  return (
+    <div className="cart">
+      <h4>Order Summary</h4>
+      <p>Selected Item: {cart.length}</p>
+      <p>Total Price: ${totalPrice} </p>
+      <p>Total Shipping: ${totalShipping} </p>
+      <p>Tax: ${tax}</p>
+      <h5>Grand Total: ${gp}</h5>
+    </div>
+  );
+};
+
+export default Cart;
--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
 const Product = (props) => {
-    // console.log(props.product)
-    const {img,name,seller,price,ratings} = props.product;
-    return (
-        <div className='product'>
-            <img src={img} alt="" />
-            <div className='product-info'>
-                <h6 className='product-name'>{name}</h6>
-                <p>Price: ${price}</p>
-                <p>Manufacturer: {seller}</p>
-                <p>Rating: {ratings}star</p>
-            </div>
-            <button className='btn-cart'>Add To  Cart</button>
-        </div>
-    );
+  // console.log(props)
+  const { img, name, seller, price, ratings } = props.product;
+  const handleAddToCart = props.handleAddToCart;
+
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings}star</p>
+      </div>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add To Cart <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
+    </div>
+  );
 };
 
 export default Product;
