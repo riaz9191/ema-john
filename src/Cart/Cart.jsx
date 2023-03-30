be060f0f523b091b5de70a2609ac12655a2a3645
riaz9191 <riazahammed9191@gmail.com>
2023-03-30 12:00:46 +0600

    show dataa from card

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -6,18 +6,25 @@ const Cart = ({ cart }) => {
   console.log(cart);
   let totalPrice = 0;
   let totalShipping = 0;
-  
+  let quantity = 0;
+
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
-    totalShipping = totalShipping + product.shipping;
+    // if(product.quantity===0){
+    //     product.quantity = 1
+    // }
+    // product.quantity = product.quantity || 1;
+    
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping  ;
+    quantity = quantity + product.quantity;
   }
-  const tax = totalPrice *7 /100;
+  const tax = (totalPrice * 7) / 100;
   const grandTotal = totalPrice + totalShipping + tax;
   const gp = grandTotal.toFixed(2);
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Item: {cart.length}</p>
+      <p>Selected Item: {quantity}</p>
       <p>Total Price: ${totalPrice} </p>
       <p>Total Shipping: ${totalShipping} </p>
       <p>Tax: ${tax}</p>
--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Cart from "../../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 import Product from "../Product/Product";
 import "./Shop.css";
 const Shop = () => {
@@ -10,9 +11,44 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(()=>{
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    // console.log(storedCart)
+    //s1: get id
+    for(const id in storedCart){
+      // console.log(id)
+      //s2 : get product from product state using id
+      const addedProduct = products.find(product=> product.id ===id);
+      // console.log(addedProduct)
+      if(addedProduct){
+        //s3: get quantity pf the product || add quantity
+      const quantity = storedCart[id];
+      addedProduct.quantity=quantity
+      //s4: add the added product to saved cart
+      savedCart.push(addedProduct)
+      }
+    }
+    //s5: set the cart
+    setCart(savedCart)
+  },[products])
   const handleAddToCart = (product) => {
-    const newCart = [...cart,product];
-    setCart(newCart)
+    let newCart = [];
+    // const newCart = [...cart,product];
+    //if product doesn't exist in the cart,then set quantity = 1;
+    //if exist update quantity by 1
+    const exists = cart.find(pd=>pd.id===product.id);
+    if(!exists){
+      product.quantity = 1;
+      newCart= [...cart,product]
+    }else{
+      exists.quantity = exists.quantity +1;
+      const remaining = cart.filter(pd=> pd.id!==product.id);
+      newCart = [...remaining,exists]
+    }
+
+    setCart(newCart);
+    addToDb(product.id)
   };
   return (
     <div className="shop-container">
